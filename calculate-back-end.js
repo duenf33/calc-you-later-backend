44873b7a59e5f116d54c27a2e9243c61f69e23d8
fernando duenas <fernando39615@aim.com>
2020-10-21 15:59:06 -0500

    last commit

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,25 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const calculate = function(value1, value2, sign){
+  const num1 = parseInt(value1, 10);
+  const num2 = parseInt(value2, 10);
+  const char = sign.toLowerCase();
+  if(char === 'x' || char === 'multiply' || char === 'times' || char === 'multiplied by'){
+  return num1 * num2;
+  } else if(char === '+' || char === 'plus' || char === 'add' || char === 'added to'){
+  return num1 + num2;
+  } else if(char === '/' || char === 'divide' || char === 'over' || char === 'divided by'){
+  return num1 / num2;
+  } else if(char === '-' || char === 'subtract' || char === 'minus' || char === 'subtracted from'){
+  return num1 - num2;
+  } else if(char === '%' || char === 'mod' || char === 'modulus'){
+  return num1 % num2;
+  } else {
+    return 'Sorry, that\'s not a mathematical operation!'
+  }  
+}
 
-
- 
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
